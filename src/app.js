3aff5cb37d3b1c3ada5c99f471c76bb12297934a
Leonid Zychkov <buldog52@gmail.com>
2018-12-12 01:04:35 +0200

    Started to code .motivation

--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ const $ = window.$ = window.jQuery = require('jquery');
 
 import './components/main-header/main-header';
 import './components/menu-toggle/menu-toggle'
-import './components/modal/modal';
-import './components/form/form';
+import './components/sticky-menu/sticky-menu'
+
